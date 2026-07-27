005fb64b9f8948de9bb9b06b1e0770a8b9a3c6a3
RevealingLeadstoHealing <info@revealing-leads-to-healing-wellness-services.org>
2026-07-27 14:27:37 -0400

    Redirect to '/rlth-preferred.html' on Home page
    Signed-off-by: RevealingLeadstoHealing <info@revealing-leads-to-healing-wellness-services.org>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
-      <iframe 
-        src="/rlth-preferred.html" 
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        title="Revealing Leads to Healing Wellness Services, LLC"
-      />
-    </div>
-  );
+  redirect('/rlth-preferred.html');
 }
