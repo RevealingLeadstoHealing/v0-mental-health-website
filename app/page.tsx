0a6975e6ed573be1976cb9247c49de9a8ce8abef
RevealingLeadstoHealing <info@revealing-leads-to-healing-wellness-services.org>
2026-07-30 21:06:32 -0400

    Trigger redeploy with an empty commit
    Signed-off-by: RevealingLeadstoHealing <info@revealing-leads-to-healing-wellness-services.org>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,2 @@
-export default function Home() {
-  return (
-    <main className="w-full h-screen">
-      <iframe
-        src="/rlth-preferred.html"
-        className="w-full h-full border-0"
-        title="Revealing Leads to Healing Wellness Services"
-      />
-    </main>
-  );
-}
+git commit --allow-empty -m "Trigger redeploy"
+git push origin main
