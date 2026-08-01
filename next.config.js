30d35c20a1cc1d38e970394afe67e9a0d0211125
RevealingLeadstoHealing <info@revealing-leads-to-healing-wellness-services.org>
2026-07-31 21:49:46 -0400

    Update public Cognito environment values to the deployed us-east-1 user pool

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_AWS_REGION: "us-east-2",
-    NEXT_PUBLIC_COGNITO_USER_POOL_ID: "us-east-2_kSd3RAPsl",
-    NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID: "64q7036m6i0sl68t9an6dqksnn",
+    NEXT_PUBLIC_AWS_REGION: "us-east-1",
+    NEXT_PUBLIC_COGNITO_USER_POOL_ID: "us-east-1_BNbtAMN95",
+    NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID: "20r3lfn9rtsh6qr3k2q3slk82u",
     NEXT_PUBLIC_RLTH_AWS_FOUNDATION_READY: "true",
   },
 };
